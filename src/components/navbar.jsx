056844f55b6f66f4f46c18dc7496356e1c68613f
bhushanamvenkatesh <venkat7596@gmail.com>
2024-03-15 10:42:43 +0530

    implemented responsive navbar by adding bootstrap cdn links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,30 +3,21 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return <div>
-        <nav className="bar navbar navbar-expand-lg navbar-light">
-            <div class="container-fluid">
-                <Link class="navbar-brand uname" href="/"><i>Venkatesh Bhushanam</i></Link>
+             <nav class="navbar bar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid ">
+                <Link class="navbar-brand uname" to="/"><i>Venkatesh Bhushanam</i></Link>
+                <button class="navbar-toggler h-button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
+                    <div className="navbar-nav ">
+                        <a className="text-white nav-link active" href="./Resume_Venkatesh Bhushanam_Edupoly2.pdf" target="_blank">About</a>
+                        <Link to='/' class="text-white nav-link active" aria-current="page">Home</Link>
+                        <Link to='/skills' class="nav-link" >Skills</Link>
+                        <Link to='/work' class="nav-link" >Work</Link>
+                        <Link to='/contact' class="nav-link" >Contact</Link>
 
-                <div class="d-flex justify-content-end collapse navbar-collapse ml-auto" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <Link to='/' class="text-white nav-link active" aria-current="page">Home</Link>
-                        </li>
-                        <li class="nav-item">
-                            <a className="text-white nav-link active" href="./Resume_Venkatesh Bhushanam_Edupoly2.pdf" target="_blank">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <Link to='/skills' class="nav-link" >Skills</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link to='/work' class="nav-link" >Work</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link to='/contact' class="nav-link" >Contact</Link>
-
-                        </li>
-
-                    </ul>
+                    </div>
                 </div>
             </div>
         </nav>
